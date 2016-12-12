0f5748dd0c63e5b424171e748351681e3fd38b10
CharlesKasper <7ckasper@stpatrick.org>
2016-12-12 13:56:25 -0600

    first day of finals

---
+++ First-Semester-Final-master/cart.js
@@ -0,0 +1,12 @@
+class Cart{
+  //What is the first part of every class? Type it below
+  constructor(l,q){
+this.itemList=l[]
+this.itemQuantity=[];
+
+
+}
+  //Type the instance functions below this comment.
+addItem()
+
+}
---
+++ First-Semester-Final-master/item.js
@@ -0,0 +1,13 @@
+class Item{
+  //Fill in the item class below this comment.
+  constructor(n,p,s){
+this.name=n
+ths.price=p
+this.shipping=a
+}
+}
+
+//Create your three test items below this comment.
+new Item lego("lego",40,2);
+new Item kh("kingdomHearts",60,3);
+new Item music("cd",30,2);
