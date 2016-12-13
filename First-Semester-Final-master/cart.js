306704b85ee721b5879a574224a8bb490bb6bfed
CharlesKasper <7ckasper@stpatrick.org>
2016-12-13 13:55:15 -0600

    day 2 of working on final
    finished the addcart function

--- First-Semester-Final-master/cart.js
+++ First-Semester-Final-master/cart.js
@@ -1,12 +1,13 @@
 class Cart{
   //What is the first part of every class? Type it below
   constructor(l,q){
-this.itemList=l[]
+this.itemList=[];
 this.itemQuantity=[];
-
 
 }
   //Type the instance functions below this comment.
-addItem()
-
-}
+addItem(i,q)
+this.itemList.unshift(i);
+this.itemQuantity.unshift(q);
+totalCart()
+var total=0
