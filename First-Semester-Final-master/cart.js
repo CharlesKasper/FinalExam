676ccfc5b84f62067cd23c00a1100a6a882213cb
CharlesKasper <7ckasper@stpatrick.org>
2016-12-15 13:53:12 -0600

    Add files via upload

--- First-Semester-Final-master/cart.js
+++ First-Semester-Final-master/cart.js
@@ -6,13 +6,24 @@ this.itemQuantity=[];
 
 }
   //Type the instance functions below this comment.
-addItem(i,q)
+addItem(i,q){
 this.itemList.unshift(i);
 this.itemQuantity.unshift(q);
+}
 
-totalCart()
+totalCart(){
 var total=0
 for (a=0; a<this.itemList[].length;a++){
 var total= var total+ this.itemList[a].price*this.itemQuantity[a];
+}
+return total
+}
+subcart(days){
+  let subcart= new Cart()
+for (d=0; d<this.itemList[].length;d++){
+  if (this.itemList[d].shipping==days){
+    subcart.itemList.unshift(this.itemList[d]);
+    subcart.itemQuantity.push(this.itemQuantity[c]);
+  }
 }
 }
--- First-Semester-Final-master/password.js
+++ First-Semester-Final-master/password.js
@@ -1,10 +1,19 @@
 class Password{
   //The first part of the class.
-
-
+constructor(k,p){
+this.publicKey=k
+this.privatekey=p
+}
 
   //Instance functions below this comment.
+validPublicKey()
 
+
+
+
+
+
+validPrivateKey()
 
 
   //Static function below this comment.
